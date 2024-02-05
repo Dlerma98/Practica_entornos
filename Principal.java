8537bbc82c6c5593b7fc8559e5e119eeae4db16d
smrv21-10 <smrv21-10@c16x14.iescierva.net>
2024-02-05 19:33:20 +0100

    metodo para introducir usuarios

--- Principal.java
+++ Principal.java
@@ -1,11 +1,21 @@
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.Scanner;
+
 
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
 		mostrarHora();
+
+		int numero = 3;
+		Usuario[] resultado = generarAlumnos(numero);
+		System.out.println("Lista de usuarios\n-----------------\n");
+		for (int i = 0; i < resultado.length; i++) {
+				System.out.println((i + 1) + " Usuario" + " Nombre completo:" + resultado[i].nombre + " "+ resultado[i].apellidos + " Email " + resultado[i].email);
+		}
 		 
+
 	}
 	public static void mostrarHora() {
 		LocalDateTime locaDate = LocalDateTime.now();
@@ -16,5 +26,27 @@ public class Principal {
 		int minutos = locaDate.getMinute();
 		int segundos = locaDate.getSecond();
 		System.out.println("Dia: " + dia + " " + mes + " " + año + " Hora actual : " + horas  + ":"+ minutos +":"+segundos); 
+	}
+
+	public static Usuario[] generarAlumnos(int num) {
+		Usuario[] usuarios = new Usuario[num];
+	
+		Scanner teclado = new Scanner(System.in);
+
+		for (int i = 0; i < usuarios.length; i++) {
+			
+			Usuario usuario = new Usuario();
+			System.out.println((i + 1) + " Usuario " + "Introduce nombre: ");
+			usuario.nombre = teclado.nextLine().trim();
+
+			System.out.println((i + 1) + " Usuario " + "Introduce apellidos: ");
+			usuario.apellidos = teclado.nextLine().trim();
+
+			System.out.println((i + 1) + " Usuario " + "Introduce email: ");
+			usuario.email = teclado.nextLine().trim();
+
+			usuarios[i] = usuario;
+		}
+		return usuarios;
 	}
 }
