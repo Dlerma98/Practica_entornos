1053e256dcb5b333b1d85df4b4717aee2108391a
smrv21-10 <smrv21-10@c16x14.iescierva.net>
2024-02-05 18:58:28 +0100

    Modificacion de Principal para que nos indique fecha y hora

--- Principal.java
+++ Principal.java
@@ -1,5 +1,20 @@
+import java.time.LocalDateTime;
+import java.time.Month;
+
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
+		mostrarHora();
+		 
+	}
+	public static void mostrarHora() {
+		LocalDateTime locaDate = LocalDateTime.now();
+		int dia = locaDate.getDayOfMonth();
+		Month mes = locaDate.getMonth();
+		int año = locaDate.getYear();
+		int horas  = locaDate.getHour();
+		int minutos = locaDate.getMinute();
+		int segundos = locaDate.getSecond();
+		System.out.println("Dia: " + dia + " " + mes + " " + año + " Hora actual : " + horas  + ":"+ minutos +":"+segundos); 
 	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Practica_entornos
+
+Programa el cual nos muestra el mensaje Hola mundo junto con la fecha y hora actual
